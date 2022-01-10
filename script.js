15331d155339f98eee8910c934f879525ade6257
Muhammad Alif Septa Fadhila Putra <88221961+alifsputra@users.noreply.github.com>
2022-01-10 19:43:20 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
-function serviceFunction(id) {
-    var x =document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += "w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// Accordion width layanan
+// function myFunction(id) {
+//     var x = document.getElementById(id);
+//     if (x.className.indexOf("w3-show") == -1) {
+//         x.className += " w3-show";
+//     } else {
+//         x.className = x.className.replace(" w3-show", "");
+//     }
+// }
+
+// Accordion full-width
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className =
+    x.previousElementSibling.className.replace("w3-black", "w3-red");
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className =
+    x.previousElementSibling.className.replace("w3-red", "w3-black");
+  }
 }
